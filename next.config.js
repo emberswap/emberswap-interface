785cf3e63b255a9572cf8a36115bffbfb822d293
bchpete <96666546+emberswap@users.noreply.github.com>
2022-03-31 23:45:52 +0200

    remove sentry

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-const { withSentryConfig } = require('@sentry/nextjs')
+//const { withSentryConfig } = require('@sentry/nextjs')
 
 const nextConfig = {
   webpack: (config) => {
@@ -112,21 +112,21 @@ const nextConfig = {
   },
 }
 
-const SentryWebpackPluginOptions = {
+//const SentryWebpackPluginOptions = {
   // Additional config options for the Sentry Webpack plugin. Keep in mind that
   // the following options are set automatically, and overriding them is not
   // recommended:
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
 
-  silent: true, // Suppresses all logs
+//  silent: true, // Suppresses all logs
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
+//}
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(withPWA(withBundleAnalyzer(nextConfig)), SentryWebpackPluginOptions)
+module.exports = withPWA(withBundleAnalyzer(nextConfig))
 
 // Don't delete this console log, useful to see the config in Vercel deployments
 console.log('next.config.js', JSON.stringify(module.exports, null, 2))
--- sentry.client.config.js
+++ sentry.client.config.js
@@ -1,24 +1,24 @@
-// This file configures the initialization of Sentry on the browser.
-// The config you add here will be used whenever a page is visited.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
-import * as Sentry from '@sentry/nextjs'
-
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
-
-Sentry.init({
-  dsn: SENTRY_DSN || 'https://88bf503a585143a6bd8f4a71d3950a41@o1182012.ingest.sentry.io/6298260',
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-  beforeSend(event, hint) {
-    // Check if it is an exception, and if so, show the report dialog
-    if (event.exception) {
-      Sentry.showReportDialog({ eventId: event.event_id })
-    }
-    return event
-  },
-})
+//// This file configures the initialization of Sentry on the browser.
+//// The config you add here will be used whenever a page is visited.
+//// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+//
+//import * as Sentry from '@sentry/nextjs'
+//
+//const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
+//
+//Sentry.init({
+//  dsn: SENTRY_DSN || 'https://88bf503a585143a6bd8f4a71d3950a41@o1182012.ingest.sentry.io/6298260',
+//  // Adjust this value in production, or use tracesSampler for greater control
+//  tracesSampleRate: 1.0,
+//  // ...
+//  // Note: if you want to override the automatic release value, do not set a
+//  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+//  // that it will also get attached to your source maps
+//  beforeSend(event, hint) {
+//    // Check if it is an exception, and if so, show the report dialog
+//    if (event.exception) {
+//      Sentry.showReportDialog({ eventId: event.event_id })
+//    }
+//    return event
+//  },
+//})
