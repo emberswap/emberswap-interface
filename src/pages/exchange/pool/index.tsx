e0e7f77685af606a4a648a0faad7f609cf254693
bchpete <96666546+emberswap@users.noreply.github.com>
2022-05-17 15:25:04 +0200

    css

--- src/pages/exchange/pool/index.tsx
+++ src/pages/exchange/pool/index.tsx
@@ -106,7 +106,7 @@ export default function Pool() {
           title={i18n._(t`Liquidity Provider Rewards`)}
           message={i18n._(t`Liquidity providers earn a 0.20% fee on all trades proportional to their share of
                         the pool. Fees are added to the pool, accrue in real time and can be claimed by
-                        withdrawing your liquidity`)}
+                        withdrawing your liquidity.`)}
           type="information"
         /> }
 
